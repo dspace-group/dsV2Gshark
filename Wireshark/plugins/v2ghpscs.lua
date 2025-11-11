81d31b61f902a400e241b150f5737fd1aa1eccab
Jens.Heilig <jens.heilig@esystems-mtg.de>
2025-11-11 12:24:06 +0100

    Add "ISO15118" to CP State display

--- Wireshark/plugins/v2ghpscs.lua
+++ Wireshark/plugins/v2ghpscs.lua
@@ -211,7 +211,11 @@ local function dissect_cp_state_ind(buf, pinfo, root, freq, dutycycle, voltage, 
         subtree:add(f_acmax, ac_max_current):append_text("A")
         pinfo.cols.info = "CP State: " .. cp_state .. " [AC max current: " .. ac_max_current .. "A]"
     else
-        pinfo.cols.info = "CP State: " .. cp_state
+        local cp_iso = ""
+        if dutycycle > 4 and dutycycle < 6 then
+            cp_iso = " [" .. dutycycle .. "% - ISO15118]"
+        end
+        pinfo.cols.info = "CP State: " .. cp_state .. cp_iso
     end
     pinfo.cols.protocol = "HomePlug AV (Ext)"
     return buf:len()
